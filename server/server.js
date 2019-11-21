245c68ea1d5ff0802587a90e496011b8f201a1f3
Meriem <mmlys89@gmail.com>
2019-11-21 18:23:02 +0000

    Create express server and shopify API end point

--- server/server.js
+++ server/server.js
@@ -1,11 +1,15 @@
+require('dotenv').config();
 const express = require('express');
-const app = express();
-const port = process.env.PORT || 5000;
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const api = require('./api');
 
-// console.log that your server is up and running
-app.listen(port, () => console.log(`Listening on port ${port}`));
+const server = express(); // create a server
 
-// create a GET route
-app.get('/express_backend', (req, res) => {
-  res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' });
-});
+server.use(bodyParser.json());
+server.use(cors());
+
+server.use('/api', api);
+
+const port = process.env.PORT || 4000;
+server.listen(port, () => console.log(`Server is running on port ${port}`));
