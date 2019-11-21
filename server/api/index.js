23612056b2226cc79913365ad5486765fdc4c7e0
Meriem <mmlys89@gmail.com>
2019-11-21 20:56:44 +0000

    Fix merge conflict,

--- server/api/index.js
+++ server/api/index.js
@@ -1,8 +1,29 @@
+const Shopify = require('shopify-api-node');
 const express = require('express');
+const axios = require('axios');
+
+const { SHOP_NAME, API_KEY, PASSWORD } = process.env;
 const router = express.Router();
 
-router.get('/shopify', (req, res) => {
-	res.send('Test');
+const shopify = new Shopify({
+	shopName: SHOP_NAME,
+	apiKey: API_KEY,
+	password: PASSWORD,
+});
+
+shopify.on('callLimits', limits => console.log(limits, 'test2'));
+
+router.get('/shopify', async (req, res) => {
+	const url = 'https://weglimpse.co/admin/api/2019-10/reports.json';
+	try {
+		if (!res.headersSent) res.json({}, 0, 500);
+		const data = await axios.get(url);
+		if (data) {
+			res.sendStatus(200).json(data);
+		}
+	} catch (error) {
+		res.sendStatus(500).json(error);
+	}
 });
 
 module.exports = router;
--- server/server.js
+++ server/server.js
@@ -13,3 +13,26 @@ server.use('/api', api);
 
 const port = process.env.PORT || 4000;
 server.listen(port, () => console.log(`Server is running on port ${port}`));
+const app = express();
+const path = require('path');
+const port = process.env.PORT || 5000;
+
+// create a GET route
+app.get('/express_backend', (req, res) => {
+	res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' });
+});
+
+/**
+ * In development environemnt, we use the create-react-app dev server.
+ * In production, the static build is served from here
+ */
+//
+if (process.env.NODE_ENV !== 'development') {
+	app.use('/', express.static(path.resolve(__dirname, '../client/build')));
+	app.get('*', (req, res) => {
+		res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+	});
+}
+
+// console.log that your server is up and running
+app.listen(port, () => console.log(`Listening on port ${port}`));
