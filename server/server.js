acc256fc0578830bfaa9961672c6dbd9a345632a
Ahmed Merehil <ahmed8merehil@yahoo.com>
2019-11-22 12:42:13 +0000

    Added Health check endpoint

--- server/server.js
+++ server/server.js
@@ -9,6 +9,10 @@ app.get('/express_backend', (req, res) => {
   res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' });
 });
 
+
+app.get('/healthcheck', (req, res) => { 
+	res.sendStatus(200) });
+
 /**
  * In development environemnt, we use the create-react-app dev server.
  * In production, the static build is served from here
