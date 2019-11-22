5c077cb1425cb0026d27f7473926ac038735813e
Daniel Grant <daniel.grant@skyscanner.net>
2019-11-22 09:25:52 +0000

    Prefix shopify env variables

--- server/api/index.js
+++ server/api/index.js
@@ -1,31 +1,31 @@
-const Shopify = require('shopify-api-node');
-const express = require('express');
-const axios = require('axios');
+const Shopify = require("shopify-api-node");
+const express = require("express");
+const axios = require("axios");
 
 const { SHOP_NAME, API_KEY, PASSWORD } = process.env;
 const router = express.Router();
 
 const shopify = new Shopify({
-	shopName: SHOP_NAME,
-	apiKey: API_KEY,
-	password: PASSWORD,
+  shopName: SHOPIFY_SHOP_NAME,
+  apiKey: SHOPIFY_API_KEY,
+  password: SHOPIFY_PASSWORD
 });
 
-shopify.on('callLimits', limits => console.log(limits, 'test2'));
+shopify.on("callLimits", limits => console.log(limits, "test2"));
 
-router.post('/transaction', async (req, res) => {
-	console.log(req.body);
-	const url = 'https://weglimpse.co/admin/api/2019-10/reports.json';
-	try {
-		const { data } = await axios.get(url);
+router.post("/transaction", async (req, res) => {
+  console.log(req.body);
+  const url = "https://weglimpse.co/admin/api/2019-10/reports.json";
+  try {
+    const { data } = await axios.get(url);
 
-		if (data) {
-			res.sendStatus(200).json(data);
-		}
-	} catch (error) {
-		console.log(error);
-		res.sendStatus(500).json(error);
-	}
+    if (data) {
+      res.sendStatus(200).json(data);
+    }
+  } catch (error) {
+    console.log(error);
+    res.sendStatus(500).json(error);
+  }
 });
 
 module.exports = router;
