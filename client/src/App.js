67349f767d306d1071facc26ccb2278213f6b473
Ahmed Merehil <ahmed8merehil@yahoo.com>
2019-11-23 21:49:29 +0000

    Create react app websocket stage 2

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import socketIOClient from "socket.io-client";
 // const socketLink = socketIOClient('http://localhost:5000');
@@ -9,49 +8,32 @@ class App extends Component {
     super();
     this.state = {
       data: null,
-      endpoint: 'http://localhost:5000/api/transaction'
+      response: "",
+      endpoint: "http://localhost:5000"
     };
   }
 
-  // componentDidMount() {
-  //     // Call our fetch function below once the component mounts
-  //     function socket(item) {
-  //       socketLink.on('timer', timestamp => item(null, timestamp));
-  //       socketLink.emit('socket', 1000);
-  //     }
-  //     console.log(socket);
-      
       componentDidMount() {
         const { endpoint } = this.state;
         const socket = socketIOClient(endpoint);
-        socket.on("totalPrice", data => this.setState({ response: data }));
+        socket.on("fromAPI", data => console.log("Sattusssssss",data.status))//this.setState({ response: data }));
         console.log(socket);
+        console.log("here ");
+        
       }
-      
-      
-    // Fetches our GET route from the Express server. (Note the route we are fetching matches the GET route from server.js
-    msg = async () => {
-    const response = await fetch('/');
-    const body = await response.json();
-
-    if (response.status !== 200) {
-      throw Error(body.message) 
-    }
-    return body;
-  };
+    
 
   render() {
+    const {response} = this.state;
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        // Render the newly fetched data inside of this.state.data 
-        <p className="App-intro">{this.state.data}</p>
-        <p className="App-intro"> This is the timer value: {this.state.endpoint} </p>
-    
-      </div>
+      <div style={{ textAlign: "center" }}>
+      {response
+          ? <p>
+            The temperature in Florence is: {response} °F
+          </p>
+          : <p>Loading...</p>}
+    </div>
+
     );
   }
 }
--- server/server.js
+++ server/server.js
@@ -8,7 +8,13 @@ const axios = require("axios");
 const path = require('path');
 const http = require ('http').createServer(app);
 const io = require('socket.io')(http);
+// const io = require('socket.io-client')('http://localhost:3000');
 
+// const socketIo = require("socket.io");
+// const index = require("./routes/index");
+// app.use(index);
+
+// const getApiAndEmit = "TODO"
 
 
 app.use(bodyParser.json());
@@ -18,26 +24,37 @@ app.use(cors());
 app.use('/api', api.router);
 const port = process.env.PORT || 5000;
 
-app.get('/', (req, res) => {
-	res.sendFile(__dirname + '/index.html');
-});
+// app.get('/', (req, res) => {
+// 	res.sendFile(__dirname + '/index.html');
+// });
 
 app.get('/express_backend', (req, res) => {
 	res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' });
 });
 
+let interval;
 io.on("connection", socket => {
-	console.log("User connected"), setInterval(
-	  () => getAPIdata(socket), 10000);
-	socket.on("disconnect", () => console.log("User disconnected"));
+  console.log("User connected");
+  if (interval) {
+	clearInterval(interval);
+  }
+  interval = setInterval(() => getApiAndEmit(socket), 10000);
+  socket.on("disconnect", () => {
+	console.log("User disconnected");
   });
+});
 
-  const getAPIdata = async socket => {
-	api.data.then(totalPrice => {
-		socket.emit("totalPrice", totalPrice); 
-	  })
-
+  const getApiAndEmit = async socket => {
+	try {
+	  const res = await axios.get(
+		console.log("axious striks")
+	  ); // Getting the data from DarkSky
+	  socket.emit("FromAPI", res);
+	} catch (error) {
+	  console.error(`Error: ${error}`);
+	}
   };
+  
 
 
 // io.on("connection", socket => {
@@ -57,12 +74,13 @@ io.on("connection", socket => {
  * In production, the static build is served from here
  */
 //
-if (process.env.NODE_ENV !== 'development') {
-	app.use('/', express.static(path.resolve(__dirname, '../client/build')));
-	app.get('*', (req, res) => {
-		res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
-	});
-}
 
+// if (process.env.NODE_ENV !== 'development') {
+// app.use('/', express.static(path.resolve(__dirname, '../client/build')));
+// 	app.get('*', (req, res) => {
+// 		res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+// 	});
+// }
 // console.log that your server is up and running
+
 http.listen(port, () => console.log(`Listening on port ${port}`));
