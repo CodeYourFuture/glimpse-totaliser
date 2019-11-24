b91e9de08d8931f328bf8df0fa4c22d4015e9962
Daniel Grant <daniel.grant@skyscanner.net>
2019-11-24 10:41:33 +0000

    Get client/server websocket connection working and tidy up

--- client/src/App.js
+++ client/src/App.js
@@ -1,39 +1,34 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 import socketIOClient from "socket.io-client";
-// const socketLink = socketIOClient('http://localhost:5000');
+
+const socket = socketIOClient("/");
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      data: null,
-      response: "",
-      endpoint: "http://localhost:5000"
+      response: ""
     };
   }
 
-      componentDidMount() {
-        const { endpoint } = this.state;
-        const socket = socketIOClient(endpoint);
-        socket.on("fromAPI", data => console.log("Sattusssssss",data.status))//this.setState({ response: data }));
-        console.log(socket);
-        console.log("here ");
-        
-      }
-    
+  componentDidMount() {
+    socket.on("FromAPI", data => {
+      console.log("received data from server");
+      this.setState({ response: data });
+    });
+  }
 
   render() {
-    const {response} = this.state;
+    const { response } = this.state;
     return (
       <div style={{ textAlign: "center" }}>
-      {response
-          ? <p>
-            The temperature in Florence is: {response} °F
-          </p>
-          : <p>Loading...</p>}
-    </div>
-
+        {response ? (
+          <p>The temperature in Florence is: {response} °F</p>
+        ) : (
+          <p>Loading...</p>
+        )}
+      </div>
     );
   }
 }
--- server/server.js
+++ server/server.js
@@ -1,86 +1,44 @@
-require('dotenv').config();
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const api = require('./api');
+require("dotenv").config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const api = require("./api");
 const app = express();
 const axios = require("axios");
-const path = require('path');
-const http = require ('http').createServer(app);
-const io = require('socket.io')(http);
-// const io = require('socket.io-client')('http://localhost:3000');
-
-// const socketIo = require("socket.io");
-// const index = require("./routes/index");
-// app.use(index);
-
-// const getApiAndEmit = "TODO"
-
+const path = require("path");
+const http = require("http").createServer(app);
+const io = require("socket.io")(http);
 
 app.use(bodyParser.json());
 app.use(cors());
 
-
-app.use('/api', api.router);
+app.use("/api", api.router);
 const port = process.env.PORT || 5000;
 
-// app.get('/', (req, res) => {
-// 	res.sendFile(__dirname + '/index.html');
-// });
-
-app.get('/express_backend', (req, res) => {
-	res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' });
+app.get("/express_backend", (req, res) => {
+  res.send({ express: "YOUR EXPRESS BACKEND IS CONNECTED TO REACT" });
 });
 
-let interval;
 io.on("connection", socket => {
   console.log("User connected");
-  if (interval) {
-	clearInterval(interval);
-  }
-  interval = setInterval(() => getApiAndEmit(socket), 10000);
+  setInterval(() => {
+    socket.emit("FromAPI", "50");
+  }, 1000);
   socket.on("disconnect", () => {
-	console.log("User disconnected");
+    console.log("User disconnected");
   });
 });
-
-  const getApiAndEmit = async socket => {
-	try {
-	  const res = await axios.get(
-		console.log("axious striks")
-	  ); // Getting the data from DarkSky
-	  socket.emit("FromAPI", res);
-	} catch (error) {
-	  console.error(`Error: ${error}`);
-	}
-  };
-  
-
-
-// io.on("connection", socket => {
-// 	console.log("User connected"), setInterval(
-// 	  () => getAPIdata(socket), 10000);
-// 	socket.on("disconnect", () => console.log("User disconnected"));
-//   });
-
-//   const getAPIdata = async socket => {
-// 	socket.emit("FromAPI", totalPrice); // Emitting a new message. It will be consumed by the client
-// 	console.log("this socket !", socket)
-//   };
-
 
 /**
  * In development environemnt, we use the create-react-app dev server.
  * In production, the static build is served from here
  */
-//
 
-// if (process.env.NODE_ENV !== 'development') {
-// app.use('/', express.static(path.resolve(__dirname, '../client/build')));
-// 	app.get('*', (req, res) => {
-// 		res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
-// 	});
-// }
-// console.log that your server is up and running
+if (process.env.NODE_ENV !== "development") {
+  app.use("/", express.static(path.resolve(__dirname, "../client/build")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
+  });
+}
 
 http.listen(port, () => console.log(`Listening on port ${port}`));
