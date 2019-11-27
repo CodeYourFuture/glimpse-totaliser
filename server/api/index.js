42d387f93617f78742cd642ce7a9874f4baa3dcb
Meriem <mmlys89@gmail.com>
2019-11-27 23:34:12 +0000

    Get previous day total

--- server/api/index.js
+++ server/api/index.js
@@ -1,10 +1,9 @@
 const Shopify = require('shopify-api-node');
 const express = require('express');
 const router = express.Router();
-const io = require("../server");
+const io = require('../server');
 
 const { SHOPIFY_SHOP_NAME, SHOPIFY_API_KEY, SHOPIFY_PASSWORD } = process.env;
-
 
 const shopify = new Shopify({
 	shopName: SHOPIFY_SHOP_NAME,
@@ -12,42 +11,64 @@ const shopify = new Shopify({
 	password: SHOPIFY_PASSWORD,
 });
 
-const callShopify = () => {
+const getTotalPrice = (orders, day) => {
+	const currentDay = new Date().getDate();
+	return orders.reduce((acc, value) => {
+		const createdAt = new Date(value.created_at).getDate();
+
+		if (day && typeof day === 'number' && day < currentDay && day > 0 && day === createdAt) {
+			console.log(value.created_at);
+			return acc + Number(value.total_price);
+		}
+		if (day === currentDay || !day) {
+			return acc + Number(value.total_price);
+		}
+		return acc + 0;
+	}, 0);
+};
+
+const callShopify = (date, day) => {
+	return shopify.order
+		.list({ financial_status: 'paid', created_at_min: date })
+		.then(orders => {
+			return {
+				totalPrice: getTotalPrice(orders, day),
+			};
+		})
+		.catch(err => console.error(err));
+};
+
+io.on('connection', socket => {
+	console.log('User connected');
+	const day = new Date().getDate() - 1;
+	const currentDate = getDate(day);
+	callShopify(currentDate, day).then(data => {
+		io.emit('Total', data.totalPrice);
+	});
+	socket.on('disconnect', () => {
+		console.log('User disconnected');
+	});
+});
+
+const getDate = day => {
 	const newDate = new Date();
 	const year = newDate.getFullYear();
 	const month = newDate.getMonth();
-	const day = newDate.getDate();
-	const currentDate = new Date(year, month, day).toISOString(); // Return orders only form current day
-	
-	console.log('Time', currentDate);
-	return shopify.order
-	.list({ financial_status: 'paid', created_at_min: currentDate })
-	.then(orders => {
-		const totalPrice = orders.reduce((acc, value, index) => {
-			return acc + Number(value.total_price);
-		}, 0);
-		return {
-			totalPrice,
-		};
-	})
-	.catch(err => console.error(err));
+	const customDay = newDate.getDate();
+	if (typeof day === 'number' && day < customDay && day > 0) {
+		const currentDate = new Date(year, month, day).toISOString();
+		return currentDate;
+	}
+	const currentDate = new Date(year, month, customDay).toISOString();
+	return currentDate;
 };
 
-io.on("connection", socket => {
-	console.log("User connected");
-	callShopify().then( data => {
-		io.emit("Total", data.totalPrice );
-	   })
-	socket.on("disconnect", () => {
-		console.log("User disconnected");
-	});	
-});
-
 router.post('/transaction', (req, res) => {
-	callShopify().then( data => {
-	 io.emit("Total", data.totalPrice );
-	res.sendStatus(200);
-	})
+	const currentDate = getDate();
+	callShopify(currentDate).then(data => {
+		io.emit('Total', data.totalPrice);
+		res.sendStatus(200);
+	});
 });
 
 module.exports = router;
